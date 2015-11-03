aac27eb677ae47f99c26c97a66d162d5c09a50bb
Sean Canlas <igotnocans@users.noreply.github.com>
2015-11-02 20:14:23 -0500

    Update main.cpp

--- RiskMain/RiskMain/main.cpp
+++ RiskMain/RiskMain/main.cpp
@@ -22,6 +22,34 @@ void main()
 	theGD.gameSetup();
 	//phaseController
 
+	//Code uploaded by Sean - Sample Main
+	// 	GameDriver driver;
+
+	// int defaultArmy = 10;
+
+	// Player player1(defaultArmy, "Sean");
+	// Country canada(&player1, defaultArmy, "Canada");
+	// player1.setCountry(&canada);
+
+	// Player player2(defaultArmy, "Trisha");
+	// Country usa(&player2, defaultArmy, "USA");
+	// player2.setCountry(&usa);
+
+	// driver.addPlayers(&player1);
+	// driver.addPlayers(&player2);
+
+	// std::vector<Player*> players = driver.getPlayers();
+
+	// cout << "Display of the values." << endl;
+
+	// for(int i = 0; i < players.size(); i++) {
+	// 	cout << "Player's name: "<< players[i]->getName() << endl;
+	// 	cout << "Player's country: " << players[i]->getCountries()[0]->getName() << endl;
+	// 	cout << "Player's army: " << players[i]->getArmies() << endl;
+	// 	cout << "To be sure it's the right owner: " << players[i]->getCountries()[0]->getOwner()->getName() << endl;
+	// 	cout << "=========" << endl;
+	// }
+	
 
 
 	
