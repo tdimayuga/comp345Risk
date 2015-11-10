79e9198755d3d2d9562808b3e588a156698e0fe4
Sean Canlas <igotnocans@users.noreply.github.com>
2015-11-10 15:25:54 -0500

    Added observerable stuff to player.cpp

--- RiskMain/RiskMain/player.cpp
+++ RiskMain/RiskMain/player.cpp
@@ -26,6 +26,7 @@ Player::Player(int val, string name){
 
 Player::Player(Territory territory, int armies, std::string playerName) {
 	this->territory.push_back(territory);
+	armies = 0;
 	this->armies += armies;
 	continents = 0;
 	this->playerName = playerName;
@@ -53,18 +54,26 @@ void Player::setName(string name){
 }
 void Player::setTerritory(Territory territory) {
 	this->territory.push_back(territory);
+	
+	Notify();
 }
 
 void Player::addArmies(int armies) {
 	armies += armies;
+	
+	Notify();
 }
 
 void Player::removeArmies(int armies) {
 	this-> armies -= armies;
+	
+	Notify();
 }
 
 void Player::addContinents(int continent) {
 	continents += continent;
+	
+	Notify();
 }
 
 bool Player::attackTerritory(Territory territory, int armies) 
