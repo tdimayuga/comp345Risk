c6e5328f32890dbe10f097dc21d0b44e181c208b
Sean Canlas <igotnocans@users.noreply.github.com>
2015-11-10 15:20:28 -0500

    Create PlayerView.cpp

---
+++ RiskMain/RiskMain/PlayerView.cpp
@@ -0,0 +1,37 @@
+/*
+ * PlayerView.cpp
+ *
+ *  Created on: Nov 8, 2015
+ *      Author: Sean-Frankel Gaon Canlas
+ */
+
+#include "PlayerView.h"
+#include <iostream>
+
+using namespace std;
+
+PlayerView::PlayerView() {
+	player = 0;
+};
+
+PlayerView::PlayerView(Player* player) {
+	this->player = player;
+	player->Attach(this);
+}
+
+PlayerView::~PlayerView() {
+	player->Detach(this);
+}
+
+void PlayerView::Update() {
+	display();
+}
+
+void PlayerView::display() {
+	int armies = player->getArmies();
+
+	cout << endl;
+	cout << "Report for " << player->getName() << endl;
+	cout << "Armies: " << armies << endl;
+
+}
